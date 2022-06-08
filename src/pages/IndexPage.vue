3e575c1eb700276fcc3c1a02b8a84cfe1fdaab2e
daidai <dai.tamae@gmail.com>
2022-06-08 17:20:39 -0300

    Added header icon

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,16 +2,16 @@
   <q-page class="flex flex-center">
     <img
       alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
+      src="~assets/spira.svg"
       style="width: 200px; height: 200px"
-    >
+    />
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
