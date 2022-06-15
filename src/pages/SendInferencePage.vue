d65813683c4c91d993e32448094f532a332ad3e6
daidai <dai.tamae@gmail.com>
2022-06-15 02:10:17 -0300

    Removed mocked data

--- src/pages/SendInferencePage.vue
+++ src/pages/SendInferencePage.vue
@@ -70,57 +70,6 @@
 <script>
 import { ref } from 'vue'
 import { api } from '/src/boot/axios'
-const options = [
-  {
-    id: "fake_id_1",
-    name: "fake_model_1",
-    created_in: "31-12-2021",
-    version:"1.0.0",
-    description:"Descrição breve do modelo ... João amava Teresa que amava Raimundo"+
-      "que amava Maria que amava Joaquim que amava Lili,"+
-      "que não amava ninguém."+
-      "João foi para os Estados Unidos, Teresa para o convento,"+
-      "Raimundo morreu de desastre, Maria ficou para tia,"+
-      "Joaquim suicidou-se e Lili casou com J. Pinto Fernandes"+
-      "que não tinha entrado na história."
-  },
-  {
-    id: "fake_id_2",
-    name: "fake_model_2",
-    created_in: "31-12-2021",
-    version:"1.0.1",
-    description:"No meio do caminho tinha uma pedra"+
-      "tinha uma pedra no meio do caminho"+
-      "tinha uma pedra"+
-      "no meio do caminho tinha uma pedra."
-  },
-  {
-    id: "fake_id_3",
-    name: "fake_model_3",
-    created_in: "31-12-2021",
-    version:"2.1.0",
-    description: "Nunca me esquecerei desse acontecimento"+
-      "na vida de minhas retinas tão fatigadas."+
-      "Nunca me esquecerei que no meio do caminho"+
-      "tinha uma pedra"+
-      "tinha uma pedra no meio do caminho"+
-      "no meio do caminho tinha uma pedra."
-  },
-  {
-    id: "fake_id_1",
-    name: "fake_model_4",
-    created_in: "01-03-2022",
-    version:"3.1.0",
-    description: "Minha terra tem palmeiras,"+
-      "Onde canta o Sabiá;"+
-      "As aves, que aqui gorjeiam,"+
-      "Não gorjeiam como lá."+
-      "Nosso céu tem mais estrelas,"+
-      "Nossas várzeas têm mais flores,"+
-      "Nossos bosques têm mais vida,"+
-      "Nossa vida mais amores."
-  },
-];
 
 export default {
   setup() {
@@ -129,12 +78,12 @@ export default {
       age: ref(null),
       breathlessness_level: ref(null),
       uploaded_file: ref(null),
-      options,
+      options: ref([]),
     };
   },
   mounted () {
     api
-      .get('/v1/users/fake_user_id/models')
+      .get('/v1/models')
       .then(response =>{this.options=response.data.models})
   }
 };
